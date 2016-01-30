625e76b08f35e8df8a615d020a82bb7af47ba4a4
Shanan Dalton <shanan_dalton@yahoo.com>
2016-01-30 11:35:04 -0500

    Player controller and villager changes
    Player can pick up and throw villagers.
    Villager spawner creates villagers when there is a low number of active
    villagers.
    Villagers walk in random directions

---
+++ Village Wranglers/Assets/Scripts/VillagerSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VillagerSpawner : MonoBehaviour {
+
+	public GameObject VillagerPrefab;
+
+	List<Transform> huts;
+	public int totalActiveVillagersAllowed = 5;
+	int numActiveVillagers = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+		huts = new List<Transform>();
+
+		for(int i = 0; i < this.transform.childCount; i++){
+			huts.Add(this.transform.GetChild(i).transform);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(numActiveVillagers <  totalActiveVillagersAllowed){
+			spawnVillager();
+			numActiveVillagers++;
+		}
+	
+	}
+
+	private void spawnVillager(){
+		
+		//Pick a random hut
+		Vector3 villagerSpawnLoc = huts[Random.Range(0, huts.Count)].position;
+		villagerSpawnLoc.y = 1;
+		Instantiate(VillagerPrefab, villagerSpawnLoc, Quaternion.identity);
+	}
+}
