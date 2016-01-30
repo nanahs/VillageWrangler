43dd53be7f25a17c449738edf250f57ebce6e48b
Keith Lewis <kwlewis@fullsail.edu>
2016-01-29 23:26:55 -0500

    Got the timer working :D there are errors referencing "using UnityEngine.UI" but they are ghost errors

---
+++ Assets/Timer/GameTimer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour
+{
+
+	float Gametime;
+	int minutes;
+	int seconds;
+	public Text timer;
+	// Use this for initialization
+	void Start()
+	{
+		Gametime = 3 * 60; // three minutes
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		minutes = (int)(Gametime / 60.0f);
+		seconds = (int)(Gametime % 60.0f);
+		timer.text = minutes.ToString() + ':' + seconds.ToString();
+		
+
+		if (Gametime < 0)
+		{
+			// exit the game here!
+		}
+		Gametime -= Time.deltaTime;
+	}
+}
