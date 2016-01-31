2f050de49fd117d43b9d851dc9ad12249eb2b6a2
Keith Lewis <kwlewis@fullsail.edu>
2016-01-31 10:12:26 -0500

    win screens trying to ge ton same build with shanan

---
+++ Village Wranglers/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonFunctions : MonoBehaviour
+{
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void ToCredits()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+
+	public void ToMainMenu()
+	{
+		Application.LoadLevel(0);
+	}
+}
